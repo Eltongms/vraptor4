803dbad1d42e75feb3eb2978cb7a35247f9e16f5
Eltno <epggpe@gmail.com>
2024-09-30 21:46:48 -0300

    moreobjects and snapshot fixed

--- vraptor-musicjungle/src/main/java/br/com/caelum/vraptor/musicjungle/controller/MusicController.java
+++ vraptor-musicjungle/src/main/java/br/com/caelum/vraptor/musicjungle/controller/MusicController.java
@@ -47,6 +47,7 @@ import br.com.caelum.vraptor.observer.download.Download;
 import br.com.caelum.vraptor.observer.download.FileDownload;
 import br.com.caelum.vraptor.observer.upload.UploadedFile;
 import br.com.caelum.vraptor.validator.Validator;
+import com.google.common.base.MoreObjects;
 
 import com.google.common.base.Objects;
 
